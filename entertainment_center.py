ceedf3cef10ca3a201914f468d8b884748f061ed
Alexander Youngblood <arghalexander3000@gmail.com>
2017-09-03 11:58:40 -0700

    all work

---
+++ entertainment_center.py
@@ -0,0 +1,10 @@
+from media import Movie
+from fresh_tomatoes import open_movies_page
+
+walle = Movie('Wall-E', 'https://resizing.flixster.com/NUT-f5bym9L1h5q6uB2OYkTZveU=/206x305/v1.bTsxMjI3OTQxNjtqOzE3NDY3OzEyMDA7MjAzMjszMDQ4', 'https://www.youtube.com/watch?v=alIq_wG9FNk')
+up = Movie('Up', 'https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_film%29.jpg', 'https://www.youtube.com/watch?v=pkqzFUhGPJg')
+avatar = Movie('Avatar', 'http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp', 'https://www.youtube.com/watch?v=cRdxXPV9GNQ')
+
+movies = (walle, up, avatar)
+
+open_movies_page(movies)
