ef8d2b8327245e7257af81df5a51f1b79ee45512
Alexander Youngblood <arghalexander3000@gmail.com>
2017-09-03 12:01:46 -0700

    comments on media

--- media.py
+++ media.py
@@ -1,5 +1,9 @@
 class Movie():
 
+	'''
+	When you need to create a new instance, pass this data to the constructor. 
+	You can then call it using instance.title, or similar.
+	'''
 	def __init__(self, title, poster_image_url, trailer_youtube_url):
 		self.title = title
 		self.poster_image_url = poster_image_url
