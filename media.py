feb43bac13627705e26e002c5bd25670fd49cf49
Alexander Youngblood <arghalexander3000@gmail.com>
2017-09-03 20:48:46 -0700

    docstring

--- media.py
+++ media.py
@@ -5,7 +5,14 @@ class Movie():
 	When you need to create a new instance, pass this data to the constructor. 
 	You can then call it using instance.title, or similar.
 	'''
+	
 	def __init__(self, title, poster_image_url, trailer_youtube_url):
+		
+		'''
+		Stores the passed data in variables,
+		accessable by 'movie_name.variable'.
+		'''
+		
 		self.title = title
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
